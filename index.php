11323ac0696315ef6a400d53e364475cbbfcad1a
jc mairot <j.mairot@codeur.online>
2021-01-19 09:08:30 +0100

    commit 19012021

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-// require_once __DIR__ . '/vendor/autoload.php';
-
-require_once 'vendor/autoload.php';
-
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-=======
 // Require composer autoload
 require_once __DIR__ . '/vendor/autoload.php';
 // Create an instance of the class:
@@ -16,5 +8,4 @@ $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('Hello World');
 
 // Output a PDF file directly to the browser
->>>>>>> 060437fa0df1774eb042684a35e6e7bdf1bfbc57
 $mpdf->Output();
